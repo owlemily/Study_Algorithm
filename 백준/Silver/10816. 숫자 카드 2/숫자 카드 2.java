23e26160c17d75478b9254dba2f8feb601e06fc8
owlemily <106426522+owlemily@users.noreply.github.com>
2024-04-19 22:22:19 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 2712 ms, Memory: 359436 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,56 +1,30 @@
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        Arrays.sort(arr);
-
-        int M = sc.nextInt();
+        HashMap<Integer, Integer> map = new HashMap<>();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            int key = sc.nextInt();
-            sb.append(upperBound(arr, key)-lowerBound(arr, key)).append(" ");
+        int N = sc.nextInt();
+        for(int i =0; i<N; i++){
+            int n = sc.nextInt();
+            if(map.get(n) == null){
+                map.put(n, 1);
+            }
+            else{
+                map.put(n, map.get(n)+1);
+            }
+        }
+        int M = sc.nextInt();
+        for(int i =0; i<M; i++){
+            int m = sc.nextInt();
+            if(map.containsKey(m)){
+                sb.append(map.get(m)).append(' ');
+            }
+            else{
+                sb.append(0).append(" ");
+            }
         }
         System.out.println(sb);
     }
-
-    static int lowerBound(int[] arr, int key){
-            int low = 0;
-            int high = arr.length;
-
-            while(low< high){
-                int mid = (low+high)/2;
-                if(key <= arr[mid]){
-                    high = mid;
-                }
-                else{
-                    low = mid+1;
-                }
-            }
-            return low;
-    }
-
-    static int upperBound(int[] arr, int key){
-            int low = 0;
-            int high = arr.length;
-
-            while(low < high){
-                int mid = (low+high)/2;
-                if(key < arr[mid]){
-                    high = mid;
-                }
-                else{
-                    low = mid+1;
-                }
-            }
-            return low;
-    }
-
 }
