3cb1cc65747f945b51b7b09be52bac2b56cfd58f
owlemily <106426522+owlemily@users.noreply.github.com>
2024-05-02 16:03:54 +0900

    [Silver IV] Title: 동전 0, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i = 0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+        int count = 0; //동전 개수세기
+
+        for(int i = N-1; i>=0; i--){
+            if (arr[i] <= K) {
+                count += (K / arr[i]);
+                K = K % arr[i];
+            }
+
+        }
+        System.out.println(count);
+    }
+}
