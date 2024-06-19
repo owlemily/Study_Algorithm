8fc1b18a0d24835cdcc25755e297289b0b27217f
owlemily <106426522+owlemily@users.noreply.github.com>
2024-06-19 19:56:09 +0900

    [level 1] Title: 추억 점수, Time: 0.04 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i<name.length; i++){
+            map.put(name[i], yearning[i]);
+        }
+        
+        int[] answer = new int[photo.length];
+        for(int i = 0; i < photo.length; i++){
+            int sum = 0;
+            for(int j = 0; j<photo[i].length; j++){
+                if(map.containsKey(photo[i][j])){
+                    sum += map.get(photo[i][j]);
+                }                
+            }
+            answer[i] = sum;
+        }
+        
+        
+        return answer;
+    }
+}
