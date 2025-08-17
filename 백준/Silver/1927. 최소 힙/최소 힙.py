fadabec962da67bfc7a68eb36494129c310ca773
Sujin Lee 이수진 <106426522+owlemily@users.noreply.github.com>
2025-08-17 23:21:03 +0900

    [Silver II] Title: 최소 힙, Time: 108 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+input = sys.stdin.readline
+N = int(input())
+q = []
+for i in range(N):
+    x = int(input())
+    if x == 0: 
+        if len(q) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(q))
+    else:
+        heapq.heappush(q, x)
+
+
