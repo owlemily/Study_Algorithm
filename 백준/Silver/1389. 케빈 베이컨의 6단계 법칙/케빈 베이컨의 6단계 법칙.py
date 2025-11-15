7c69d0e34311b856c957d910ba05b3bf3ffaff9b
Sujin Lee 이수진 <106426522+owlemily@users.noreply.github.com>
2025-11-16 00:06:44 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 344 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,28 +1,25 @@
-import sys
-INF = int(1e9)
-N, M = map(int,input().split())
-graph =[[INF] * (N + 1)  for _ in range(N + 1)]
-
-for _ in range(M):
-    a,b = map(int,sys.stdin.readline().rstrip().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
-
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
+import heapq
+N, M = map(int, input().split())
+INF = 1e9
+graph = [[INF]*(N+1) for _ in range(N+1)]
+for i in range(1, N+1):
+    for j in range(1, N+1):
         if i == j:
             graph[i][j] = 0
+for _ in range(M):
+    A, B = map(int, input().split())
+    graph[A][B] = 1
+    graph[B][A] = 1
 
-for k in range(1, N + 1):
-    for i in range(1, N + 1):
-        for j in range(1 , N + 1):
-            graph[i][j] = min(graph[i][j],graph[i][k] + graph[k][j])
 
-bacon = INF
-answer = 0
-for i in range(N, 0, -1):
-    s = sum(graph[i][1:])
-    if bacon >= s:
-        bacon = s
-        answer = i
-print(answer)
+for k in range(1, N+1): #거쳐가는 노드수
+    for i in range(1, N+1): #출발
+        for j in range(1, N+1): #도착
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+answer = []
+for i in range(1, N+1):
+    heapq.heappush(answer, (sum(graph[i]), i))
+
+print(heapq.heappop(answer)[1])
+    
