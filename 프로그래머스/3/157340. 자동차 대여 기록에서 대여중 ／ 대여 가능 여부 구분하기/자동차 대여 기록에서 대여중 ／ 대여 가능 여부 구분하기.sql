0a0b21e926dd2fcc613f9114ad6053f77a6b6132
Sujin Lee 이수진 <106426522+owlemily@users.noreply.github.com>
2026-04-25 16:51:51 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -4,15 +4,11 @@ else '대여 가능' end as AVAILABILITY
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY
 order by CAR_ID DESC*/
 
--- 차 목록과 대여 히스토리를 분리해서 생각할 필요가 있다. 
-select CAR_ID,
-case when exists(
-    select 1
-    from CAR_RENTAL_COMPANY_RENTAL_HISTORY h
-    where h.CAR_ID = car.CAR_ID 
-    and ((start_date = '2022-10-16') 
-         or (end_date = '2022-10-16')
-        or (start_date < '2022-10-16' and end_date >= '2022-10-16'))
-) then '대여중' else '대여 가능' end as AVAILABILITY
-from (select distinct CAR_ID from CAR_RENTAL_COMPANY_RENTAL_HISTORY) car
-order by CAR_ID DESC
+WITH base AS(SELECT CAR_ID,
+    CASE WHEN START_DATE <= '2022-10-16' and END_DATE >= '2022-10-16' then 1 else 0 end as AVAILABLE_CNT
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+)
+SELECT CAR_ID, CASE WHEN SUM(AVAILABLE_CNT) >= 1 then '대여중' ELSE '대여 가능' end as AVAILABILITY
+from base
+group by car_id
+order by car_id desc
